c795060862fc861f1033e5d382992b9c89f84640
farazhaider <farazhaider88@yahoo.com>
2018-10-30 01:11:51 +0500

    added home celll

---
+++ Zaha/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Zaha
+//
+//  Created by Faraz Haider on 30/10/2018.
+//  Copyright © 2018 Faraz Haider. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
